cdedab93a2186b3c0da45a24012e5918fbe5273b
bahtiyorovabduhalilboy-sys <bahtiyorovabduhalilboy@gmail.com>
2026-09-25 17:19:24 +0500

    configure postgresql with dj-database-url

--- imtihon/settings.py
+++ imtihon/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,14 +159,9 @@ MAILERS = {
 }
 
 DEFAULT_FROM_EMAIL = 'bahtiyorovabduhalilboy@gmail.com'
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'avtosalon_db',
-        'USER': 'user',
-        'PASSWORD': 'arch',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
